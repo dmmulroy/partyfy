43506d22a57bb8fc4d0185fbf58d98e4479d8012
Dillon Mulroy <dillon.mulroy@formidable.com>
2019-08-14 22:26:53 -0400

    start working on buffer/stream api

--- index.js
+++ index.js
@@ -17,36 +17,37 @@ const colors = [
   '#ff6968'
 ];
 
-// const rgbColors = [
-//   [255, 141, 139],
-//   [254, 214, 137],
-//   [136, 255, 137],
-//   [135, 255, 255],
-//   [139, 181, 254],
-//   [215, 140, 255],
-//   [255, 140, 255],
-//   [255, 104, 247],
-//   [254, 108, 183],
-//   [255, 105, 104]
-// ];
+const rgbColors = [
+  [255, 141, 139],
+  [254, 214, 137],
+  [136, 255, 137],
+  [135, 255, 255],
+  [139, 181, 254],
+  [215, 140, 255],
+  [255, 140, 255],
+  [255, 104, 247],
+  [254, 108, 183],
+  [255, 105, 104]
+];
 
-function partyfy(inputStream, outputStream, options = {}) {}
+function mix(color, overlayedColor, opacity = 50) {
+  return {
+    r: (overlayedColor.r - color.r) * (opacity / 100) + color.r,
+    g: (overlayedColor.g - color.g) * (opacity / 100) + color.g,
+    b: (overlayedColor.b - color.b) * (opacity / 100) + color.b
+  };
+}
 
-async function main() {
+async function partyfy(inputBuffer, outputStream, options = {}) {
   try {
-    const image = await Jimp.read(
-      path.join(__dirname, 'input_images', 'urwelcome.jpg')
-    );
+    const image = await Jimp.read(inputStream);
 
     image.greyscale();
 
     const gifEncoder = new GifEncoder(image.bitmap.width, image.bitmap.height);
 
-    gifEncoder.pipe(
-      fs.createWriteStream(
-        path.join(__dirname, 'output_images', 'urwelcome.png')
-      )
-    );
+    gifEncoder.pipe(outputStream);
+
     gifEncoder.setDelay(75);
 
     gifEncoder.setRepeat(0); // loop forever
@@ -60,25 +61,29 @@ async function main() {
     colors.forEach(color => {
       const clonedImage = image.clone();
 
-      if (clonedImage.hasAlpha()) {
-        clonedImage.scan(
-          0,
-          0,
-          clonedImage.bitmap.width,
-          clonedImage.bitmap.height,
-          (x, y, idx) => {
-            const transparent = clonedImage.bitmap.data[idx + 3] < 1;
+      clonedImage.scan(
+        0,
+        0,
+        clonedImage.bitmap.width,
+        clonedImage.bitmap.height,
+        (x, y, idx) => {
+          const transparent = clonedImage.bitmap.data[idx + 3] < 1;
 
-            if (transparent) {
-              clonedImage.setPixelColor(Jimp.rgbaToInt(0, 255, 0, 0), x, y);
-            } else {
-              clonedImage.setPixelColor(Jimp.cssColorToHex(color), x, y);
-            }
+          if (transparent) {
+            clonedImage.setPixelColor(Jimp.rgbaToInt(0, 255, 0, 0), x, y);
+          } else {
+            const currentColor = Jimp.intToRGBA(
+              clonedImage.getPixelColor(x, y)
+            );
+
+            const partyColor = Jimp.intToRGBA(Jimp.cssColorToHex(color));
+
+            const { r, g, b, a = 1 } = mix(currentColor, partyColor);
+
+            clonedImage.setPixelColor(Jimp.rgbaToInt(r, g, b, a), x, y);
           }
-        );
-      } else {
-        clonedImage.color([{ apply: 'mix', params: [color] }]);
-      }
+        }
+      );
 
       gifEncoder.addFrame(clonedImage.bitmap.data);
       gifEncoder.flushData();
@@ -87,6 +92,26 @@ async function main() {
     gifEncoder.finish();
   } catch (err) {
     console.error(err);
+
+    throw new Error('partyfy error: ', err);
+  }
+}
+
+async function main() {
+  try {
+    const imageFile = fs.readFileSync(
+      path.join(__dirname, 'input_images', 'input.png')
+    );
+
+    console.log(imageFile);
+
+    const ouptputFile = fs.createWriteStream(
+      path.join(__dirname, 'output_images', 'output.png')
+    );
+
+    await partyfy(imageFile, ouptputFile);
+  } catch (err) {
+    console.log(err);
   }
 }
 
