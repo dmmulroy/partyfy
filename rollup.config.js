f8dbbb7044f07a00adc259c86cc040c5239ffd2f
Dillon Mulroy <dillon.mulroy@formidable.com>
2019-08-20 21:30:09 -0400

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,3 @@
-const resolve = require('rollup-plugin-node-resolve');
-const commonjs = require('rollup-plugin-commonjs');
-const json = require('rollup-plugin-json');
-
 const pkg = require('./package.json');
 
 module.exports = [
@@ -10,7 +6,6 @@ module.exports = [
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' }
-    ],
-    plugins: [resolve({ preferBuiltins: true }), commonjs(), json()]
+    ]
   }
 ];
