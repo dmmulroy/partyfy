4e5853d8d365491198dab6d768e191b045aa142d
Dillon Mulroy <dillon.mulroy@formidable.com>
2019-08-20 23:12:19 -0400

    update bin stuff

---
+++ bin/partyfy-cli.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+const path = require('path');
+const fs = require('fs');
+
+const partyfy = require('../src/partyfy');
+
+(async () => {
+  const [, , inputFile, outputFile] = process.argv;
+  try {
+    const imageFile = fs.readFileSync(path.resolve(inputFile));
+
+    const partyImage = await partyfy(imageFile);
+
+    fs.writeFileSync(path.resolve(outputFile), partyImage);
+    console.log(`${outputFile} succesfully created!`);
+  } catch (err) {
+    console.log(err);
+  }
+})();
